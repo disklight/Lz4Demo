c59b645716161faf7dc4c86ebcb45515c1ab5de7
shenhao <disklight@outlook.com>
2020-09-03 11:39:21 +0800

    add a time record for compress/decompress

--- Lz4Demo/Lz4Demo/MainWindow.xaml.cs
+++ Lz4Demo/Lz4Demo/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Windows;
 using LZ4;
+using HiPerfMetrics;
 
 namespace Lz4Demo
 {
@@ -17,15 +18,23 @@ namespace Lz4Demo
 
         private void Compressbtn_Click(object sender, RoutedEventArgs e)
         {
+            HiPerfMetric hiPerfMetric = new HiPerfMetric("Compress");
+            hiPerfMetric.Start();
             var result = LZ4Codec.Wrap(Encoding.UTF8.GetBytes(textboxforlz4.Text));
             textboxforlz4.Text = Convert.ToBase64String(result);
+            hiPerfMetric.Stop();
+            TimeResult.Text = "compress time spend time(s) is:" + hiPerfMetric.TotalTimeInSeconds.ToString();
         }
 
         private void Decompressbtn_Click(object sender, RoutedEventArgs e)
         {
-            var result1 = Encoding.UTF8.GetString(
+            HiPerfMetric hiPerfMetric = new HiPerfMetric("Decompress");
+            hiPerfMetric.Start();
+            textboxforlz4.Text = Encoding.UTF8.GetString(
                     LZ4Codec.Unwrap(Convert.FromBase64String(textboxforlz4.Text)));
-            textboxforlz4.Text = result1;
+            hiPerfMetric.Stop();
+            TimeResult.Text = "compress time spend time(s) is:" + hiPerfMetric.TotalTimeInSeconds.ToString();
+
         }
     }
 }
